c2796c5c4c86a7b9ec53538b83dcd3ff610cee5c
kanika sharma <kanikasharma@kanikas-MacBook-Air.local>
2025-09-26 14:08:34 +0400

    familty-view-added

---
+++ src/app/family-view/family-view/family-view.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+interface Child {
+  name: string;
+  gender:string
+}
+
+interface Wife {
+  name: string;
+  children: Child[];
+}
+
+interface Family {
+  name: string;
+  wives: Wife[];
+}
+@Component({
+  selector: 'app-family-view',
+  templateUrl: './family-view.component.html',
+  styleUrls: ['./family-view.component.less']
+})
+export class FamilyViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+    familyData: Family = {
+    name: 'ABDULLA MOHAMED AHMED MUFTAH ALNASRI',
+    wives: [
+      {
+        name: 'HUDA MOUSA ABDALLA MOHAMED MOUSA ',
+        children: [
+          { name: 'Zayed',gender:"boy" },
+          { name: 'Shama',gender:"girl" }
+        ]
+      },
+      {
+        name: 'SALMA SAEED W.O.ALI MUBARAK AL MANSOURI',
+        children: [
+          { name: 'Layek',gender:"boy" },
+          { name: 'Nora',gender:"girl" },
+        ]
+      }
+    ]
+  };
+
+
+    familyData2: Family = {
+    name: 'AHMED MOHAMMED SALEH BUQARAA ALHEMEIRI',
+    wives: [
+      {
+        name: 'MERVAT GABER AL-MADANYA',
+        children: [
+          { name: 'Jaber',gender:"boy" },
+          { name: 'Alia',gender:"girl" }
+        ]
+      }
+    ]
+  };
+}
+
+
+
+  
